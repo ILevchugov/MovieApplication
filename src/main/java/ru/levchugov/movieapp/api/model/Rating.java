3d240cabcdebaf0cecc60d6f7c92dc056e34e3b4
levchugov <i.levchugov@g.nsu.ru>
2020-09-29 14:30:47 +0700

    travis-ci add

--- src/main/java/ru/levchugov/movieapp/api/model/Rating.java
+++ src/main/java/ru/levchugov/movieapp/api/model/Rating.java
@@ -1,8 +1,6 @@
 package ru.levchugov.movieapp.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/ru/levchugov/movieapp/security/AudienceValidator.java
+++ src/main/java/ru/levchugov/movieapp/security/AudienceValidator.java
@@ -1,16 +1,15 @@
 package ru.levchugov.movieapp.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+@AllArgsConstructor
 class AudienceValidator implements OAuth2TokenValidator<Jwt> {
-    private final String audience;
 
-    AudienceValidator(String audience) {
-        this.audience = audience;
-    }
+    private final String audience;
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
